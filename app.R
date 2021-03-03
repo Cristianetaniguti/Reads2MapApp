cdcdc911f6592525a80219f53bb2615924acc826
cristianetaniguti <chaytaniguti@gmail.com>
2021-03-03 00:52:02 -0300

    docker ok

--- app.R
+++ app.R
@@ -8,11 +8,3 @@ Reads2MapApp::Reads2MapApp() # add parameters here (if any)
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
-
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
-
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-Reads2MapApp::run_app() # add parameters here (if any)
